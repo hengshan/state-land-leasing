9d71af4db56d995abad535fbb450b7d50bfcd923
hengshan <hengshan.li@gmail.com>
2022-05-20 14:03:57 +0800

    add cookiecutter folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Singapore State Land Leasing and Valuation Predicative Model',
+    author='SLA',
+    license='',
+)
